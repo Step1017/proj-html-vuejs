4fbde5b38e5f50495bd3e049acdae467a5810042
Step1017 <s.aveta@yahoo.com>
2023-02-14 16:00:24 +0100

    Scaffolding base

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  name: "App",
+  components: {
+    AppHeader, 
+    AppMain, 
+    AppFooter
+  }
+};
+</script>
+
+<template>
+  
+</template>
+
+<style>
+
+</style>
